99342f3896aa129afc234af2e6d1a2a81b3a2dac
kakutani <kakutani@aka.jp>
2017-03-24 18:37:49 +0900

    update

--- api/views.py
+++ api/views.py
@@ -1,14 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Todo
 from .serializers import TodoSerializer
-# import os
-# from django.conf import settings
-# from django.http.response import HttpResponse
-#
-# def index(_):
-#     html = open(
-#         os.path.join(settings.STATICFILES_DIRS[0], "index.html")).read()
-#     return HttpResponse(html)
+import os
+from django.conf import settings
+from django.http.response import HttpResponse
+
+def index(_):
+    html = open(
+        os.path.join(settings.STATICFILES_DIRS[0], "index.html")).read()
+    return HttpResponse(html)
 
 class TodoViewSet(ModelViewSet):
     queryset = Todo.objects.all()
--- django_rest_todo/urls.py
+++ django_rest_todo/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from api import views
-# from api.views import *
+from api.views import *
 
 router = DefaultRouter()
 router.register(r'todos', views.TodoViewSet)
@@ -27,6 +27,6 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url('', RedirectView.as_view(url='/static/index.html')),
-    # url(r'', index, name='index'),
+    # url('', RedirectView.as_view(url='/static/index.html')),
+    url(r'', index, name='index'),
 ]
